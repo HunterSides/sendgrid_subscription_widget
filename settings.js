c07204faedf51a534b2ba66739618c9f654f5423
Hunter Sides <56932084+HunterSides@users.noreply.github.com>
2022-07-13 12:13:55 -0500

    update heroku settings

--- settings.js
+++ settings.js
@@ -1,13 +1,15 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = "https://sg-subscription-widget.herokuapp.com/";
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "hunter.s.1337@gmail.com";
+exports.senderName = "Hunter Sides";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
-
+exports.listId = "29916fc3-c035-4039-bd88-c6f3d02a5667"; //Developer newsletter
+exports.listId = "47b7176a-980f-42b9-928e-afb05720b3cd"; //community newsletter
+exports.listId = "ba8f318b-556e-4930-896a-1c64ec57b58f"; //MNO newsletter
+exports.listId = "3d0db80e-11db-410a-8dc2-589a5f0af522"; //Investor newsletter
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
 exports.templateId = null;
